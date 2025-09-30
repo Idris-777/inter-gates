6414632ee4f83e18e03d76dc8ae0075c32253d66
Idris <uddinmahi156@gmail.com>
2025-09-30 21:53:12 +0530

    added proper controls for movement in mobiles

--- script.js
+++ script.js
@@ -45,7 +45,28 @@ document.body.addEventListener("mousedown", (e) => {
   }
 
   if(e.target.parentNode.classList.contains("controls")){
-    console.log(document.querySelector(".gate").style.top)
+    let current_gate = e.target.parentNode.parentNode
+    console.log(current_gate)
+
+    let pos_top = parseInt(window.getComputedStyle(current_gate).getPropertyValue("top") )
+    let pos_left = parseInt(window.getComputedStyle(current_gate).getPropertyValue("left"))
+
+    if(e.target.classList.contains("X")){
+          current_gate.style.left = pos_left+10+"px"
+    }
+    else if(e.target.classList.contains("-X")){
+          current_gate.style.left = pos_left-10+"px"
+    }
+    else if(e.target.classList.contains("Y")){
+          current_gate.style.top = pos_top-10+"px"
+    }
+    else{
+      current_gate.style.top = pos_top+10+"px"
+    }
+
+    
+    console.log(pos_top)
+    
   }
    
 
